5b4aa0bc2effc91d5e04a68e48aa4842229f9067
Jérôme Mahuet <jerome.mahuet@gmail.com>
2015-06-05 08:20:55 +0200

    hw2

--- 2/HW02.hs
+++ 2/HW02.hs
@@ -1,7 +1,6 @@
 {-# OPTIONS_GHC -Wall #-}
 module HW02 where
 
-import Data.List ((\\))
 
 -- Mastermind -----------------------------------------
 
@@ -24,29 +23,38 @@ colors = [Red, Green, Blue, Yellow, Orange, Purple]
 -- Exercise 1 -----------------------------------------
 
 -- Get the number of exact matches between the actual code and the guess
+--exactMatches :: Code -> Code -> Int
+--exactMatches [] _ = 0
+--exactMatches (_ : _) [] = 0
+--exactMatches (x:xs) (y:ys) = count + exactMatches xs ys
+--  where count = if x == y then 1 else 0
+
 exactMatches :: Code -> Code -> Int
-exactMatches xs ys = length $  xs \\ ys
+exactMatches c1 c2 = length $ filter (uncurry (==)) $ zip c1 c2
 
 -- Exercise 2 -----------------------------------------
 
 -- For each peg in xs, count how many times is occurs in ys
 countColors :: Code -> [Int]
-countColors = undefined
+countColors xs = map (\c -> length $ filter (==c) xs) colors
 
 -- Count number of matches between the actual code and the guess
 matches :: Code -> Code -> Int
-matches = undefined
+matches c1 c2 = sum $ zipWith min (countColors c1) (countColors c2)
 
 -- Exercise 3 -----------------------------------------
 
 -- Construct a Move from a guess given the actual code
 getMove :: Code -> Code -> Move
-getMove = undefined
+getMove s g = Move g exact (allMatches-exact)
+  where exact = exactMatches s g
+        allMatches = matches s g
 
 -- Exercise 4 -----------------------------------------
 
 isConsistent :: Move -> Code -> Bool
-isConsistent = undefined
+isConsistent (Move c e a) c' = e == e' && a == a'
+  where (Move _ e' a') = getMove c c'
 
 -- Exercise 5 -----------------------------------------
 
